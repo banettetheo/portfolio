a88a46b232086ebf39232386fcd5ba6c6b5ba888
banettetheo <theobanette@hotmail.fr>
2025-08-06 20:55:38 +0200

    add Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import Header from "@/app/_components/Header";
+import Footer from "@/app/_components/Footer";
+import Hero from "@/app/_components/Hero";
 
 export default function Home() {
   return (
-    <Header></Header>
+      <div className="bg-blue-300">
+        <Header/>
+          <Hero/>
+          <Footer/>
+      </div>
   );
 }

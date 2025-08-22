d05f1b6c0fb82c0609492e146aeaf47a6cc797a5
theobanette <theobanette@hotmail.fr>
2025-08-22 23:20:50 +0200

    fix lang icon

--- src/app/[locale]/_components/Header.tsx
+++ src/app/[locale]/_components/Header.tsx
@@ -6,7 +6,7 @@ import {Moon, Sun} from "lucide-react";
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
 import {useTheme} from "next-themes";
 import {Button} from "@/components/ui/button";
-import {useChangeLocale} from "@/locales/client";
+import {useChangeLocale, useCurrentLocale} from "@/locales/client";
 
 const navigation = [
     { name: 'Projects', href: '#projects' },
@@ -35,45 +35,49 @@ export default function Header() {
                     </Link>
                     <h1 className="ml-4 content-center">Théo Banette</h1>
                 </div>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button className="m-2" variant="outline" size="icon">
-                            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                            <span className="sr-only">Toggle theme</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem onClick={() => setTheme("light")}>
-                            Light
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => setTheme("dark")}>
-                            Dark
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => setTheme("system")}>
-                            System
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button className="m-2" variant="outline" size="icon">
-                            <img src="/us.svg" alt="Light" className="size-4"/>
-                            <img src="/fr.svg" alt="Light" className="size-4"/>
-                            <span className="sr-only">Toggle theme</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem onClick={() => changeLocale('en')}>
-                            <img src="/us.svg" alt="US" className="size-4"/>
-                            <p>Anglais</p>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => changeLocale('fr')}>
-                            <img src="/fr.svg" alt="FR" className="size-4"/>
-                            <p>Français</p>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <div>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className="m-2" variant="outline" size="icon">
+                                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                                <span className="sr-only">Toggle theme</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                                Light
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                                Dark
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("system")}>
+                                System
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className="m-2" variant="outline" size="icon">
+                                { useCurrentLocale() == 'en' ? (
+                                        <img src="/us.svg" alt="US" className="size-4"/>
+                                    ) : (
+                                        <img src="/fr.svg" alt="FR" className="size-4"/>
+                                    )}
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => changeLocale('en')}>
+                                <img src="/us.svg" alt="US" className="size-4"/>
+                                <p>Anglais</p>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => changeLocale('fr')}>
+                                <img src="/fr.svg" alt="FR" className="size-4"/>
+                                <p>Français</p>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </nav>
         </header>
     )
